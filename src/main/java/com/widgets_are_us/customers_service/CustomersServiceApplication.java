c62c3596cc5d303a237efac3d8f155c5b8a0d20b
Jon Hatfield <johatfie@gmail.com>
2023-03-22 11:00:38 -0400

    optimize imports

--- src/main/java/com/widgets_are_us/customers_service/CustomersServiceApplication.java
+++ src/main/java/com/widgets_are_us/customers_service/CustomersServiceApplication.java
@@ -2,8 +2,8 @@ package com.widgets_are_us.customers_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
--- src/main/java/com/widgets_are_us/customers_service/controllers/HelloWorldController.java
+++ src/main/java/com/widgets_are_us/customers_service/controllers/HelloWorldController.java
@@ -4,8 +4,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
 @RestController
