af3f14ddf2a77924b0107bbbd14b2e017ab0149e
Jon Hatfield <johatfie@gmail.com>
2023-03-27 18:40:34 -0400

    remove debugging commented code

--- src/test/java/com/widgets_are_us/customers_service/CustomersServiceApplicationTests.java
+++ src/test/java/com/widgets_are_us/customers_service/CustomersServiceApplicationTests.java
@@ -3,7 +3,6 @@ package com.widgets_are_us.customers_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest(classes = {CustomerServiceApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @SpringBootTest
 class CustomersServiceApplicationTests {
 
