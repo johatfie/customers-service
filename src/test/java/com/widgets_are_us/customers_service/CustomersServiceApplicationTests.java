5a8ae5efcf962a7e08fe2face3d99a5a744e838c
Jon Hatfield <johatfie@gmail.com>
2023-03-27 18:06:41 -0400

    rename CustomerServiceApplicationTests

--- src/test/java/com/widgets_are_us/customers_service/CustomersServiceApplicationTests.java
+++ src/test/java/com/widgets_are_us/customers_service/CustomersServiceApplicationTests.java
@@ -3,8 +3,9 @@ package com.widgets_are_us.customers_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//@SpringBootTest(classes = {CustomerServiceApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @SpringBootTest
-class CustomerServiceApplicationTests {
+class CustomersServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
